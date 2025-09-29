f3b000291780ced3da0d01f674c18e7d64d2f2d1
Nícolas Gabriel Silveira Fernandes <nicolas.fernandes02072009@gmail.com>
2025-09-29 16:41:05 -0300

    erewrgwrfvwrfwwvw

---
+++ src/br/ulbra/DAO/ClienteDAO.java
@@ -0,0 +1,108 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.ulbra.DAO;
+
+import br.ulbra.Model.Cliente;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author aluno.saolucas
+ */
+public class ClienteDAO extends AbstractDAO implements CrudRepository<Cliente> {
+
+    @Override
+    public void salvar(Cliente c) throws SQLException {
+        String sql = "INSERT INTO dbcliente (nome, email, telefone) VALUES (?, ?, ?)";
+        try (Connection con = getConnection();
+                PreparedStatement ps = con.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
+            ps.setString(1, c.getNome());
+            ps.setString(2, c.getEmail());
+            ps.setString(3, c.getTelefone());
+            int affected = ps.executeUpdate();
+            if (affected == 0) {
+                throw new SQLException("Falha ao inserir cliente.");
+            }
+            try (ResultSet rs = ps.getGeneratedKeys()) {
+                if (rs.next()) {
+                    c.setId(rs.getInt(1));
+                }
+            }
+        }
+    }
+
+    @Override
+    public Cliente buscarPorId(int id) throws SQLException {
+        String sql = "SELECT id, nome, email, telefone FROM dbcliente WHERE id = ?";
+        try (Connection con = getConnection();
+                PreparedStatement ps = con.prepareStatement(sql)) {
+            ps.setInt(1, id);
+            try (ResultSet rs = ps.executeQuery()) {
+                if (rs.next()) {
+                    return new Cliente(
+                            rs.getInt("id"),
+                            rs.getString("nome"),
+                            rs.getString("email"),
+                            rs.getString("telefone")
+                    );
+                }
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public List<Cliente> listar() throws SQLException {
+        String sql="select id, nome, email, telefone FROM dbcliente ORDER by id";
+        List<Cliente> lista = new ArrayList<>();
+        try(Connection con = getConnection();  PreparedStatement ps = con.prepareStatement(sql); 
+                ResultSet rs= ps.executeQuery()){  //aqui mosrtra e não modifica
+             while(rs.next()){
+                Cliente c = new Cliente(
+                rs.getInt("id"),
+                rs.getString("nome"),
+                rs.getString("email"),
+                rs.getString("telefone"));
+                lista.add(c);
+
+             }
+             
+        }
+        return lista;
+
+    }
+    
+
+    @Override
+    public void atualizar(Cliente c) throws SQLException {
+        String sql="UPDATE dbcliente set nome= ?, email=?, telefone=? where id=?";
+        try(Connection con= getConnection(); PreparedStatement ps = con.prepareStatement(sql)){
+            ps.setString(1,c.getNome());
+            ps.setString(2,c.getEmail());
+            ps.setString(3,c.getTelefone());
+            ps.setInt(4,c.getId());
+            ps.executeUpdate();  //mexe, modifica os objetos
+        }
+    }
+
+    @Override
+    public void remover(int id) throws SQLException {
+        String sql = "DELETE FROM dbcliente WHERE id=?";
+        try(Connection con = getConnection(); PreparedStatement ps = con.prepareStatement(sql)){
+            ps.setInt(1, id);
+            ps.executeUpdate();
+        }
+    }
+    
+
+}
---
+++ src/br/ulbra/Model/Usuario.java
@@ -0,0 +1,82 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.ulbra.Model;
+
+/**
+ *
+ * @author aluno.saolucas
+ */
+public class Usuario {
+
+    private int id;
+    private String nome;
+    private String login;
+    private String senha;
+    private boolean ativo;
+
+    public Usuario() {
+    }
+
+    public Usuario(int id, String nome, String login, String senha, boolean ativo) {
+        this.id = id;
+        this.nome = nome;
+        this.login = login;
+        this.senha = senha;
+        this.ativo = ativo;
+    }
+
+    public Usuario(int id, String nome, String login, boolean ativo) {
+        this.id = id;
+        this.nome = nome;
+        this.login = login;
+        this.ativo = ativo;
+    }
+
+    public Boolean isAtivo() {
+        return ativo;
+    }
+
+    public void setAtivo(Boolean ativo) {
+        this.ativo = ativo;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public String toString() {
+        return "cod: " + id + "Nome: " + nome + "login: " + login + "Senha: " + senha + "ativo" + ativo;
+
+    }
+}
